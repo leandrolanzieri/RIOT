f704dceedfb55521a05144b3e0a4fbc4f85680c1
Leandro Lanzieri <leandro.lanzieri@haw-hamburg.de>
2019-07-15 11:22:29 +0200

    makefiles/kconfig: Merge app & generated configurations

--- makefiles/kconfig.mk
+++ makefiles/kconfig.mk
@@ -7,16 +7,38 @@ include $(RIOTMAKE)/tools/kconfiglib.inc.mk
 # .config file will contain the generated configuraion
 export KCONFIG_CONFIG = $(GENERATED_DIR)/.config
 
+# This file will contain configurations that overwrite the generated
+# KCONFIG_CONFIG
+KCONFIG_APP_CONFIG = $(APPDIR)/app.config
+
+# This file contains the configuration merged from KCONFIG_CONFIG and
+# KCONFIG_APP_CONFIG
+KCONFIG_MERGED_CONFIG = $(GENERATED_DIR)/merged.config
+
+# Add configurations to merge, in ascendent priority (i.e. a file overrides the
+# previous ones)
+MERGE_SOURCES += $(wildcard $(KCONFIG_CONFIG))
+MERGE_SOURCES += $(wildcard $(KCONFIG_APP_CONFIG))
+
 # Opens the menuconfig interface for configuration of modules using the Kconfig
 # system.
 menuconfig: $(KCONFIG_GENERATED_DEPENDENCIES) $(MENUCONFIG)
 	@$(MENUCONFIG) $(KCONFIG)
 
+# Generates a merged configuration file from the given sources
+$(KCONFIG_MERGED_CONFIG): $(MERGECONFIG) $(MERGE_SOURCES)
+	$(Q)\
+	if ! test -z "$(strip $(MERGE_SOURCES))"; then \
+		$(MERGECONFIG) $(KCONFIG) $@ $(MERGE_SOURCES); \
+	else \
+		rm -f $@; \
+	fi
+
 # Build a header file with all the Kconfig configurations. genconfig will avoid
 # any unnecessary rewrites of the header file if no configurations changed.
-$(KCONFIG_GENERATED_AUTOCONF_HEADER_C): $(KCONFIG_GENERATED_DEPENDENCIES) $(GENCONFIG) FORCE
+$(KCONFIG_GENERATED_AUTOCONF_HEADER_C): $(KCONFIG_GENERATED_DEPENDENCIES) $(GENCONFIG) $(KCONFIG_MERGED_CONFIG) FORCE
 	@mkdir -p '$(dir $@)'
-	@$(GENCONFIG) --header-path $@ $(KCONFIG)
+	@KCONFIG_CONFIG=$(KCONFIG_MERGED_CONFIG) $(GENCONFIG) --header-path $@ $(KCONFIG)
 
 # Build a Kconfig file defining all used modules. This is done by defining
 # Kconfig variables like 'module-<module-name> = y'. Then, every module Kconfig
