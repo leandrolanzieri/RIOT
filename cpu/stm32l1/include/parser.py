7bbe9f19cbf432d0a728a27fd3c269a477e48dca
Leandro Lanzieri <leandro.lanzieri@haw-hamburg.de>
2019-08-06 17:35:52 +0200

    WIP: Generating board pinout

--- cpu/stm32l1/include/parser.py
+++ cpu/stm32l1/include/parser.py
@@ -1,31 +1,11 @@
+import json
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 import yaml
-from jinja2 import Environment, FileSystemLoader
 from peewee import *
 import peewee
 from playhouse.shortcuts import *
 
-env = Environment(
-    loader=FileSystemLoader('.'),
-    lstrip_blocks=True,
-    trim_blocks=True
-)
-
-template = env.get_template('stm32.jinja')
 db = SqliteDatabase(':memory:')
-
-with open('stm32l151x6.yml') as stream:
-    try:
-        af = yaml.safe_load(stream)
-
-    except yaml.YAMLError as exc:
-        print(exc)
-
-with open('periph_conf.yml') as stream:
-    try:
-        pc = yaml.safe_load(stream)
-
-    except yaml.YAMLError as exc:
-        print(exc)
 
 class BaseModel(Model):
     id = CharField(primary_key=True)
@@ -50,6 +30,9 @@ class CpuPin(BaseModel):
     port = CharField()
     num = IntegerField()
 
+    def get_label(self):
+        return "P{}{}".format(self.port, self.num)
+
 class Dma(BaseModel):
     type = CharField()
     device = CharField()
@@ -72,15 +55,27 @@ class Pin(ForeignKeyField):
     def __init__(self, **kwargs):
         super().__init__(CpuPin, **kwargs)
 
-class UsartPinmux(BaseModel):
+class BasePinmux(BaseModel):
+    def get_pins(self):
+        l = []
+        for k,v in self._meta.fields.items():
+            if isinstance(v, Pin):
+                l.append({'pin': getattr(self, k), 'function': k})
+        return l
+
+class UsartPinmux(BasePinmux):
     periph = Periph(Usart, backref='relation')
     tx_pin = Pin()
     rx_pin = Pin()
     tx_af = AlternateFunction()
     rx_af = AlternateFunction()
 
-    def get_pins(self):
-        return {"tx_pin": self.tx_pin, "rx_pin": self.rx_pin}
+    def get_label(self):
+        return "{}_tx_{}_{}_rx_{}_{}".format(
+                self.periph,
+                self.tx_pin.port, self.tx_pin.num,
+                self.rx_pin.port, self.rx_pin.num).lower()
+
 
 class Spi(BaseModel):
     type = CharField()
@@ -94,14 +89,11 @@ class Spi(BaseModel):
     rx_dma_stream = CharField()
     tx_dma_channel = CharField()
 
-class SpiPinmux(BaseModel):
+class SpiPinmux(BasePinmux):
     periph = ForeignKeyField(Spi)
     mosi_pin = Pin()
     miso_pin = Pin()
     af = CharField()
-
-    def get_pins(self):
-        return {"miso_pin": self.miso_pin, "mosi_pin": self.mosi_pin}
 
     def get_label(self):
         return "{}_mosi_{}_{}_miso_{}_{}".format(
@@ -124,7 +116,7 @@ class I2C(BaseModel):
     rx_dma_stream = CharField()
     rx_dma_channel = CharField()
 
-class I2CPinmux(BaseModel):
+class I2CPinmux(BasePinmux):
     periph = ForeignKeyField(I2C)
     scl_pin = Pin()
     sda_pin = Pin()
@@ -135,14 +127,10 @@ class I2CPinmux(BaseModel):
         return "{}_scl_{}_{}_sda_{}_{}".format(
                 self.periph,
                 self.scl_pin.port, self.scl_pin.num,
-                self.sda_pin.port, self.sda_pin.num)
-
-    def get_pins(self):
-        l = {"scl_pin": self.scl_pin}
-        l.update(self.pinmux.get_pins())
-        return l
+                self.sda_pin.port, self.sda_pin.num).lower()
 
 class BasePinmuxConfig(Model):
+    id = AutoField()
     class Meta:
         database = db
 
@@ -167,8 +155,15 @@ class BasePinmuxConfig(Model):
         sp.save(force_insert=True)
         return sp
 
+    @property
+    def config_group(self):
+        return self.pinmux.periph.type + str(self.id)
+
     def get_pins(self):
-        raise NotImplemented()
+        pins = self.pinmux.get_pins()
+        for p in pins:
+            p['config_group'] = self.config_group
+        return pins
 
 class I2CConfig(BasePinmuxConfig):
     pinmux = ForeignKeyField(I2CPinmux)
@@ -177,73 +172,140 @@ class I2CConfig(BasePinmuxConfig):
 class UsartConfig(BasePinmuxConfig):
     pinmux = ForeignKeyField(UsartPinmux)
 
-    def get_pins(self):
-        return self.pinmux.get_pins()
-
 class SpiConfig(BasePinmuxConfig):
-    id = AutoField()
     pinmux = ForeignKeyField(SpiPinmux)
     cs_pin = Pin()
 
     def get_pins(self):
-        l = {"cs_pin": self.cs_pin}
-        l.update(self.pinmux.get_pins())
+        l = [{'pin':self.cs_pin, 'function':'cs_pin'}]
+        l.extend(self.pinmux.get_pins())
+        for p in l:
+            p['config_group'] = self.config_group
         return l
-    
+
 class Pinout(Model):
     pin = Pin(primary_key=True)
     function = CharField()
+    config_group = CharField()
 
     class Meta:
-        db = database
+        database = db
 
-db.create_tables([UsartPinmux, Usart])
-db.create_tables([SpiPinmux, Spi])
-db.create_tables([I2CPinmux, I2C])
-db.create_tables([Dma])
-db.create_tables([CpuPin])
-db.create_tables([UsartConfig, SpiConfig, I2CConfig])
+class Pinmap(Model):
+    id = AutoField()
+    label = CharField()
+    pin = CharField()
+    connector = CharField()
 
-for el in af['pins']:
-    CpuPin.create(**el)
+    @property
+    def L(self):
+        return self.label
 
-for el in af["usart"]:
-    Usart.create(**el)
-
-for el in af["spi"]:
-    Spi.create(**el)
-
-for el in af["i2c"]:
-    I2C.create(**el)
-
-for el in af['i2c_pinmux']:
-    I2CPinmux.create(**el)
-
-for el in af['spi_pinmux']:
-    SpiPinmux.create(**el)
-
-for el in af['usart_pinmux']:
-    UsartPinmux.create(**el)
-
-for el in pc['board']['periph_conf']['i2c']:
-    I2CConfig.create(**el)
-
-for el in pc['board']['periph_conf']['spi']:
-    SpiConfig.create(**el)
-
-for el in pc['board']['periph_conf']['uart']:
-    UsartConfig.create(**el)
-
-used_pins = []
-for conf in SpiConfig.select():
-    used_pins.extend(conf.get_pins().values())
-
-for conf in I2CConfig.select():
-    used_pins.extend(conf.get_pins().values())
+    class Meta:
+        database = db
 
 
-for c in UsartConfig.select():
-    pass
-    #used_pins.extend(get_pins(c))
 
+def create_tables():
+    db.create_tables([UsartPinmux, Usart])
+    db.create_tables([SpiPinmux, Spi])
+    db.create_tables([I2CPinmux, I2C])
+    db.create_tables([Dma])
+    db.create_tables([CpuPin])
+    db.create_tables([UsartConfig, SpiConfig, I2CConfig])
+    db.create_tables([Pinout])
+    db.create_tables([Pinmap])
 
+def format_function(function, config_group):
+    return config_group + '.' + function
+
+def create_elements(cpu, board):
+    for el in cpu['pins']:
+        CpuPin.create(**el)
+
+    for el in cpu["usart"]:
+        Usart.create(**el)
+
+    for el in cpu["spi"]:
+        Spi.create(**el)
+
+    for el in cpu["i2c"]:
+        I2C.create(**el)
+
+    for el in cpu['i2c_pinmux']:
+        I2CPinmux.create(**el)
+
+    for el in cpu['spi_pinmux']:
+        SpiPinmux.create(**el)
+
+    for el in cpu['usart_pinmux']:
+        UsartPinmux.create(**el)
+
+    for el in board['board']['periph_conf']['i2c']:
+        I2CConfig.create(**el)
+
+    for el in board['board']['periph_conf']['spi']:
+        SpiConfig.create(**el)
+
+    for el in board['board']['periph_conf']['uart']:
+        UsartConfig.create(**el)
+    
+    for k, v in board['board']['pin_map'].items():
+        for el in v:
+            el['connector'] = k
+            Pinmap.create(**el)
+
+if __name__ == "__main__":
+
+    with open('stm32l151x6.yml') as stream:
+        try:
+            cpu = yaml.safe_load(stream)
+
+        except yaml.YAMLError as exc:
+            print(exc)
+
+    with open('periph_conf.yml') as stream:
+        try:
+            board = yaml.safe_load(stream)
+
+        except yaml.YAMLError as exc:
+            print(exc)
+
+    create_tables()
+    create_elements(cpu, board)
+
+    used_pins = []
+    for conf in SpiConfig.select():
+        used_pins.extend(conf.get_pins())
+
+    for conf in I2CConfig.select():
+        used_pins.extend(conf.get_pins())
+
+    for conf in UsartConfig.select():
+        used_pins.extend(conf.get_pins())
+
+    for pin in used_pins:
+        Pinout.create(**pin)
+
+    q = Pinmap.select(Pinmap.label, Pinmap.pin, Pinmap.connector,
+                      format_function(Pinout.function, Pinout.config_group) \
+                      .alias('function')) \
+              .join(Pinout, JOIN.LEFT_OUTER, on=(Pinmap.pin == Pinout.pin)).dicts()
+
+    for el in q:
+        if not board.get(el['connector']):
+            # Connector numbering starts with 1 in boards, so we add padding
+            board[el['connector']] = [None, el]
+        else:
+            board[el['connector']].append(el)
+
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        lstrip_blocks=True,
+        trim_blocks=True,
+        autoescape=select_autoescape(['xml', 'html'])
+    )
+
+    template_board = env.get_template('Nucleo-64_nv_plain.svg')
+
+    print(template_board.render(board))
