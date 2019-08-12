7b5a902ef53f15ff9f12ad6c65ca760c5f1222fd
Jose Alamos <jose.alamos@haw-hamburg.de>
2019-08-12 16:10:03 +0200

    add bindings

--- hwd/dts/arm/stm32/stm32l1/stm32l152re/dts_iterator.py
+++ hwd/dts/arm/stm32/stm32l1/stm32l152re/dts_iterator.py
@@ -6,11 +6,29 @@ import re
 db = SqliteDatabase(':memory:')
 
 _periphs = {}
-def periph(cls):
-    _periphs[cls.__name__.lower()] = cls
-    return cls
+def node(cells=[]):
+    def inner(cls):
+        d = {"CELLS": cells, "parent":ForeignKeyField(cls)}
+        for c in cells:
+            d[c] =  IntegerField()
 
-class PhandleCell(ForeignKeyField):
+        __cls = type("{}Cell".format(cls.__name__),
+                (BaseModel,), d)
+
+        db.create_tables([__cls])
+        setattr(cls, __cls.__name__, __cls)
+        _periphs[cls.__name__.lower()] = cls
+        return cls
+    return inner
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+class Cell(ForeignKeyField):
+    def __init__(self, model, **kwargs):
+        super().__init__(getattr(model, "{}Cell".format(model.__name__)), **kwargs)
+
     def db_value(self, value):
         try:
             val = value[0]
@@ -21,72 +39,81 @@ class PhandleCell(ForeignKeyField):
         args = dict(zip(self.rel_model.CELLS, value[1:]))
         return super().db_value(self.rel_model.create(parent=model, **args).id)
 
-@periph
+class Phandle(Model):
+    id = CharField(primary_key=True)
+    url = CharField()
+    class Meta:
+        database = db
+
+###############################################################################
+
+@node(cells=['stream', 'channel'])
 class Dma(Model):
     id = CharField(primary_key=True)
     device = CharField()
     class Meta:
         database = db
 
-class DmaCell(Model):
-    CELLS = ['stream', 'channel']
-    parent = ForeignKeyField(Dma)
-    stream = IntegerField()
-    channel = IntegerField()
-    class Meta:
-        database = db
-
-@periph
-class Gpio(Model):
+@node(cells=['num', 'flags'])
+class Gpio(BaseModel):
     label = CharField()
-    class Meta:
-        database = db
 
-class GpioCell(Model):
-    CELLS = ['num']
-    parent = ForeignKeyField(Gpio)
-    num = IntegerField()
-
-    class Meta:
-        database = db
-
-@periph
-class UsartPinmux(Model):
+@node()
+class UsartPinctrl(BaseModel):
     id = CharField(primary_key=True)
-    tx = PhandleCell(GpioCell)
-    tx_af = CharField()
-    rx = PhandleCell(GpioCell)
-    rx_af = CharField()
+    tx = Cell(Gpio)
+    rx = Cell(Gpio)
 
-    class Meta:
-        database = db
-
-class UsartPinmuxCell(Model):
-    CELLS = []
-    parent = ForeignKeyField(UsartPinmux)
-    class Meta:
-        database = db
-
-@periph
-class Usart(Model):
+@node()
+class Usart(BaseModel):
     id = CharField(primary_key=True)
     device = CharField()
     rcc = CharField()
     interrupts = CharField()
     isr = CharField()
     status = CharField()
-    tx_dma = PhandleCell(DmaCell)
-    rx_dma = PhandleCell(DmaCell)
-    pinmux = PhandleCell(UsartPinmuxCell, null=True)
+    tx_dma = Cell(Dma, column_name="tx-dma")
+    rx_dma = Cell(Dma, column_name="rx-dma")
+    pinctrl = Cell(UsartPinctrl, null=True)
 
-    class Meta:
-        database = db
-
-class Phandle(Model):
+@node()
+class SpiPinctrl(BaseModel):
     id = CharField(primary_key=True)
-    url = CharField()
-    class Meta:
-        database = db
+    miso = Cell(Gpio)
+    mosi = Cell(Gpio)
+    sck = Cell(Gpio)
+    cs = Cell(Gpio, null=True)
+
+@node()
+class Spi(BaseModel):
+    id = CharField(primary_key=True)
+    device = CharField()
+    rcc = CharField()
+    interrupts = CharField()
+    status = CharField()
+    tx_dma = Cell(Dma, column_name="tx-dma")
+    rx_dma = Cell(Dma, column_name="rx-dma")
+    pinctrl = Cell(SpiPinctrl, null=True)
+
+@node()
+class I2CPinctrl(BaseModel):
+    id = CharField(primary_key=True)
+    sda = Cell(Gpio)
+    scl = Cell(Gpio)
+
+@node()
+class I2C(BaseModel):
+    id = CharField(primary_key=True)
+    device = CharField()
+    rcc = CharField()
+    interrupts = CharField()
+    isr = CharField()
+    status = CharField()
+    tx_dma = Cell(Dma, column_name="tx-dma")
+    rx_dma = Cell(Dma, column_name="rx-dma")
+    pinctrl = Cell(I2CPinctrl, null=True)
+    speed = CharField()
+
 
 dtb_data = open('board.dtb', 'rb').read()
 tree = fdt.parse_dtb(dtb_data)
@@ -113,16 +140,16 @@ class Entry:
         node = {"id": model_id}
 
         for p in tree.get_node(path).props:
-            node[p.name] = p.data[0] if len(p.data) == 1 else p.data
+            if hasattr(p, 'data'):
+                node[p.name] = p.data[0] if len(p.data) == 1 else p.data
+            else:
+                node[p.name] = True
 
         return model_class.create(**node)
 
 phandles = tree.search('phandle')
 
 db.create_tables([Phandle])
-db.create_tables([DmaCell])
-db.create_tables([UsartPinmuxCell])
-db.create_tables([GpioCell])
 
 phs = {}
 for p in phandles:
@@ -136,6 +163,7 @@ for w in tree.walk():
     path = w[0]
     Entry.get_model(path)
 
+print(db.get_tables())
 print("== All USARTs ==")
 for u in Usart.select():
     print(u.device)
@@ -145,7 +173,13 @@ print("\n== All active USARTs ==")
 for u in Usart.select().where(Usart.status == 'okay'):
     print(u.device)
     print(u.status)
-    print(u.pinmux.parent.tx_af)
+    print(u.pinctrl.parent.tx)
 
-for d in DmaCell.select():
-    print("HOLA: ", d.stream)
+for u in Spi.select().where(Spi.status == 'okay'):
+    print(u.device)
+    print(u.status)
+
+for u in I2C.select().where(I2C.status == 'okay'):
+    print(u.device)
+    print(u.status)
+
