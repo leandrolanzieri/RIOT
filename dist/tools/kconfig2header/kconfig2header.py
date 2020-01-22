3048d37f2e640a6472c7c2daf1d0b3b308f40e1f
Leandro Lanzieri <leandro.lanzieri@haw-hamburg.de>
2020-01-22 15:50:33 +0100

    Adding kconfig2header.py
    Known issues:
    - Although groups are created properly some symbols are
    being placed in the parent group (e.g. USB configurations).
    
    - No an issue of the tool itself, but if left like this there will be
    name collisions with the macros that already exist in header files. This
    should be solved once configuration only live in Kconfig files.
    
    Missing features: Allow to specify the maximum depth before stop
    creating new groups.

--- dist/tools/kconfig2header/kconfig2header.py
+++ dist/tools/kconfig2header/kconfig2header.py
@@ -11,23 +11,54 @@ import os
 import re
 import sys
 import textwrap
+import logging
 import kconfiglib
 
-IGNORED_PREFIXES = ['KCONFIG']
+IGNORED_SYMBOL_PREFIXES = ['KCONFIG']
 
-def print_header(out, ingroup):
+class DocGroup(object):
+    def __init__(self, groupName, fileName=None, fileBaseDir=None):
+        self.name = groupName
+        if not fileName:
+            fileName = 'kconfig_{}.h'.format(groupName)
+        if fileBaseDir:
+            self.fileName = os.path.join(fileBaseDir, fileName)
+        else:
+            self.fileName = fileName
+
+    def openFile(self, flags='w'):
+        self.file = open(self.fileName, flags)
+
+    def closeFile(self):
+        self.file.close()
+
+    def get_formatted_title(self):
+        title = self.name.lower()
+        if title.startswith('kconfig_'):
+            title = title[len('kconfig_'):]
+        title = title.replace('_', ' ') + ' compile configurations'
+        return title.capitalize()
+
+def print_header(out, group, groupTitle=None, ingroup=None):
     """Prints the doxygen header for the header file.
 
     Args:
         out: Output file to write
         ingroup: Name of the Doxygen group to be part of
     """
-    header = ('/**\n'
-              ' * @ingroup {}\n'
-              ' * @{{\n'
-              ' */\n')
-    header = header.format(ingroup)
-    out.write(header)
+    if not groupTitle:
+        groupTitle = '{} compile configurations'.format(group)
+
+    header = ['/**']
+    header.append(' * @defgroup {} {}'.format(group, groupTitle))
+
+    if ingroup:
+        header.append(' * @ingroup {}'.format(ingroup))
+
+    header.append(' * @{')
+    header.append(' */')
+    for l in header:
+        out.write(l + '\n')
 
 def print_footer(out):
     """Prints the Doxygen footer for the header file.
@@ -47,7 +78,7 @@ def should_generate_macro(node, state):
     sym = node.item
     if isinstance(sym, kconfiglib.Symbol) and sym.type != 'UNKNOWN' and sym.nodes:
         prefix = sym.name.split('_')[0]
-        return prefix not in IGNORED_PREFIXES and sym.name not in state and node.prompt
+        return prefix not in IGNORED_SYMBOL_PREFIXES and sym.name not in state and node.prompt
     return False
 
 def format_expression(expr, skip_y=True, prefix= " if "):
@@ -55,7 +86,6 @@ def format_expression(expr, skip_y=True, prefix= " if "):
     if expr_str == '"y"' and skip_y:
         return ''
     return prefix + re.sub(r'\b([A-Z][A-Z0-9_]*)\b', r'@ref \1', expr_str)
-
 
 def generate_macro_type(item):
     """Generates a macro doc line that contains the option type
@@ -139,24 +169,93 @@ def generate_macro(node):
     lines += '#define CONFIG_{}\n'.format(sym.name)
     return lines
 
-def write_node(out, node, state):
-    """Writes the content a node and all the nested ones intou the output file
+def write_node(node, state):
+    """Writes the content a node and all the nested ones into the output file
     
     Args:
         out: Output file to write
         node: Current node to write
         state: Global state of the symbols
     """
+    if node.list:
+        if isinstance(node.item, kconfiglib.Symbol):
+            enter_group(node.item.name.lower(), state)
+        elif node.item == kconfiglib.MENU and node is not state['kconfig'].top_node:
+            enter_group(node.prompt[0].replace(' ', '_').lower(), state)
+
     if should_generate_macro(node, state):
         macro = generate_macro(node)
         for l in macro:
-            out.write(l)
+            state['stack'][-1].file.write(l)
 
     if node.list:
         entry = node.list
         while entry:
-            write_node(out, entry, state)
+            write_node(entry, state)
             entry = entry.next
+        exit_group(state)
+
+def generate_group_header(state, groupTitle=None):
+    cur = state['stack'][-1]
+    try:
+        parent = state['stack'][-2]
+    except:
+        parent = None
+    lines = ['/**']
+
+    if not groupTitle:
+        groupTitle = cur.get_formatted_title()
+
+    lines.append(' * @defgroup {} {}'.format(cur.name, groupTitle))
+
+    if parent:
+        lines.append(' * @ingroup {}'.format(parent.name))
+    lines.append(' * @{')
+    lines.append(' */')
+    return lines
+
+def enter_group(group, state, groupTitle=None, fileName=None):
+    """ Enters to a group of the given name.
+
+        This also closes the file of the actual group and opens the one of the
+        new one. If the group has already been created before if uses it and
+        does not overwrite the file.
+
+        Args:
+            group: Group name
+            state: Global state object
+            fileName: Optional name for the file of the group
+    """
+    try:
+        state['stack'][-1].closeFile()
+    except:
+        logging.debug('No parent group')
+
+    # use existant group if present
+    if group in state['groups']:
+        g = state['groups'][group]
+        g.openFile(flags='a')
+        state['stack'].append(g)
+    else:
+        g = DocGroup(group, fileName=fileName, fileBaseDir=state['output_dir'])
+        state['groups'][group] = g
+        g.openFile()
+        state['stack'].append(g)
+        for l in generate_group_header(state, groupTitle=groupTitle):
+            g.file.write(l + '\n')
+
+def exit_group(state):
+    """ Exits the current group, closes its file and opens the parent's one.
+
+        Args:
+            state: Global state object
+    """
+    try:
+        state['stack'][-1].closeFile()
+        del state['stack'][-1]
+        state['stack'][-1].openFile(flags='a')
+    except:
+        logging.debug('No more groups')
 
 def main():
     """Entry point"""
@@ -165,10 +264,12 @@ def main():
                      ' Kconfig specification.'))
     parser.add_argument('-k', '--kconfig', default='Kconfig',
         help='Kconfig file to read')
-    parser.add_argument('-o', '--output', required=True,
-        type=argparse.FileType('w'), help='Output file')
+    parser.add_argument('-o', '--outputDir', required=True,
+        help='Output directory')
     parser.add_argument('-g', '--group', default='kconfig',
         help='Doxygen group to be in')
+    parser.add_argument('-d', '--debug', dest='debug', action='store_true',
+        help='Enable debug messages')
     args = parser.parse_args()
 
     parts = os.path.split(args.kconfig)
@@ -177,14 +278,25 @@ def main():
     kconfig = kconfiglib.Kconfig(parts[1])
     os.chdir(oldwd)
 
-    # Keeps track of the state for the printed symbols
-    state = {}
+    # Configure logging
+    logLevel = logging.DEBUG if args.debug else logging.INFO
+    logging.basicConfig(level=logLevel)
 
-    print_header(args.output, args.group)
-    write_node(args.output, kconfig.top_node, state)
-    print_footer(args.output)
+    state = {
+        'groups': {}, # keeps track of created groups
+        'stack': [],  # keeps track of the stack of nested groups
+        'output_dir': args.outputDir, # output directory for files
+        'kconfig': kconfig # Kconfig instance
+    }
 
-    args.output.close()
+    # enter the root group
+    enter_group('kconfig', state, groupTitle='Kconfig configuration options')
+
+    write_node(kconfig.top_node, state)
+    for g in state['groups'].values():
+        g.openFile(flags='a')
+        print_footer(g.file)
+        g.closeFile()
 
 if __name__=='__main__':
     main()
