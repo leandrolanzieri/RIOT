497359284e4d0798ac56d92f77813d195477bc50
Lena Boeckmann <einhornhool@mobi31.inet.haw-hamburg.de>
2020-02-13 17:08:50 +0100

    cpu/kinetis: add sha1 hwa implementation

--- cpu/kinetis/periph/hash_sha1.c
+++ cpu/kinetis/periph/hash_sha1.c
@@ -18,59 +18,265 @@
  * @file
  * @brief       Implementation of the SHA-1 hashing function
  *
- * @author      Wei Dai and others
- * @author      Oliver Hahm <oliver.hahm@inria.fr>
+ * @author      Lena Boeckmann <lena.boeckmann@haw-hamburg.de>
  */
 
 #include <stdint.h>
 #include <string.h>
 
+#include "vendor/MKW21D5.h"
 #include "hashes/sha1.h"
+#include "mmcau.h"
+#include "xtimer.h"
 
-#define ENABLE_DEBUG    (1)
+#define ENABLE_DEBUG    (0)
 #include "debug.h"
+
+#define SHA1_K0  0x5a827999
+#define SHA1_K20 0x6ed9eba1
+#define SHA1_K40 0x8f1bbcdc
+#define SHA1_K60 0xca62c1d6
+
+uint32_t t_start[2], t_stop[2];
+uint32_t time[5];
 
 void sha1_init(sha1_context *ctx)
 {
-    // TBD
-    (void)ctx;
     DEBUG("SHA1 init HW accelerated implementation\n");
+    /* Initialize hash variables */
+    ctx->state[0] = 0x67452301;
+    ctx->state[1] = 0xefcdab89;
+    ctx->state[2] = 0x98badcfe;
+    ctx->state[3] = 0x10325476;
+    ctx->state[4] = 0xc3d2e1f0;
+    ctx->byte_count = 0;
+    ctx->buffer_offset = 0;
+}
+
+static void sha1_step(int count, int *i, int func, int constant, int *w)
+{
+    int index = *i;
+    for (int j = 0; j < count; j++) {
+            CAU->DIRECT[0] = MMCAU_2_CMDS((HASH+func), (ADRA+CA4));
+            CAU->ADR_CAA = constant;
+            CAU->LDR_CA[5] = w[index-16];
+            CAU->XOR_CA[5] = w[index-14];
+            CAU->XOR_CA[5] = w[index-8];
+            CAU->XOR_CA[5] = w[index-3];
+            CAU->ROTL_CA[5] = 1;
+            w[index++] = CAU->STR_CA[5];
+            CAU->DIRECT[0] = MMCAU_2_CMDS((ADRA+CA5), SHS);
+        }
+    *i = index;
+}
+
+#if ENABLE_DEBUG
+static void sha1_hash_block(sha1_context *ctx)
+{
+    int j;
+    int i = 0;
+    int w[80];
+    t_start[0] = xtimer_now_usec();
+    for (j = 0; j < 5; j++) {
+        CAU->LDR_CA[j] = ctx->state[j];
+    }
+
+    CAU->DIRECT[0] = MMCAU_1_CMD((MVRA+CA0));
+    CAU->ROTL_CAA = 5;
+
+    t_start[1] = xtimer_now_usec();
+    for (j = 0; j < 16; j++) {
+        w[i] = ctx->buffer[i];
+        CAU->DIRECT[0] = MMCAU_2_CMDS((HASH+HFC), (ADRA+CA4));
+        CAU->ADR_CAA = SHA1_K0;
+
+        CAU->ADR_CAA = w[i++];
+        CAU->DIRECT[0] = MMCAU_1_CMD(SHS);
+    }
+
+    /* perform hash algorithm on input */
+    sha1_step(4, &i, HFC, SHA1_K0, w);
+    t_stop[1] = xtimer_now_usec();
+    time[1] = t_stop[1] - t_start[1];
+
+    t_start[1] = xtimer_now_usec();
+    sha1_step(20, &i, HFP, SHA1_K20, w);
+    t_stop[1] = xtimer_now_usec();
+    time[2] = t_stop[1] - t_start[1];
+
+    t_start[1] = xtimer_now_usec();
+    sha1_step(20, &i, HFM, SHA1_K40, w);
+    t_stop[1] = xtimer_now_usec();
+    time[3] = t_stop[1] - t_start[1];
+
+    t_start[1] = xtimer_now_usec();
+    sha1_step(20, &i, HFP, SHA1_K60, w);
+    t_stop[1] = xtimer_now_usec();
+    time[4] = t_stop[1] - t_start[1];
+
+    for (j = 0; j < 5; j++) {
+        CAU->ADR_CA[j] = ctx->state[j];
+        ctx->state[j] = CAU->STR_CA[j];
+    }
+
+    t_stop[0] = xtimer_now_usec();
+    time[0] = t_stop - t_start;
+    DEBUG("Time:\nSha Step K0: %ld us\nSha Step K20: %ld us\nSha Step K40: %ld us\nSha Step K60: %ld us\nHash Block total: %ld us\n", time[1], time[2], time[3], time[4], time[0]);
+}
+#else
+static void sha1_hash_block(sha1_context *ctx)
+{
+    int j;
+    int i = 0;
+    int w[80];
+    for (j = 0; j < 5; j++) {
+        CAU->LDR_CA[j] = ctx->state[j];
+    }
+
+    CAU->DIRECT[0] = MMCAU_1_CMD((MVRA+CA0));
+    CAU->ROTL_CAA = 5;
+
+    for (j = 0; j < 16; j++) {
+        w[i] = ctx->buffer[i];
+        CAU->DIRECT[0] = MMCAU_2_CMDS((HASH+HFC), (ADRA+CA4));
+        CAU->ADR_CAA = SHA1_K0;
+
+        CAU->ADR_CAA = w[i++];
+        CAU->DIRECT[0] = MMCAU_1_CMD(SHS);
+    }
+
+    /* perform hash algorithm on input */
+    sha1_step(4, &i, HFC, SHA1_K0, w);
+    sha1_step(20, &i, HFP, SHA1_K20, w);
+    sha1_step(20, &i, HFM, SHA1_K40, w);
+    sha1_step(20, &i, HFP, SHA1_K60, w);
+
+    for (j = 0; j < 5; j++) {
+        CAU->ADR_CA[j] = ctx->state[j];
+        ctx->state[j] = CAU->STR_CA[j];
+    }
+}
+#endif /* ENABLE_DEBUG */
+
+static void sha1_add_uncounted(sha1_context *s, uint8_t data)
+{
+    uint8_t *const b = (uint8_t *) s->buffer;
+
+#ifdef __BIG_ENDIAN__
+    b[s->buffer_offset] = data;
+#else
+    b[s->buffer_offset ^ 3] = data;
+#endif
+    s->buffer_offset++;
+    if (s->buffer_offset == SHA1_BLOCK_LENGTH) {
+        sha1_hash_block(s);
+        s->buffer_offset = 0;
+    }
+}
+
+static void sha1_update_byte(sha1_context *ctx, uint8_t data)
+{
+    ++ctx->byte_count;
+    sha1_add_uncounted(ctx, data);
 }
 
 void sha1_update(sha1_context *ctx, const void *data, size_t len)
 {
-    // TBD
-    (void)ctx;
-    (void)data;
-    (void)len;
+    const uint8_t *d = data;
+    while (len--) {
+        sha1_update_byte(ctx, *(d++));
+    }
+}
+
+static void sha1_pad(sha1_context *s)
+{
+    /* Implement SHA-1 padding (fips180-2 §5.1.1) */
+    /* Pad with 0x80 followed by 0x00 until the end of the block */
+    sha1_add_uncounted(s, 0x80);
+    while (s->buffer_offset != 56) {
+        sha1_add_uncounted(s, 0x00);
+    }
+
+    /* Append length in the last 8 bytes */
+    sha1_add_uncounted(s, 0);                   /* We're only using 32 bit lengths */
+    sha1_add_uncounted(s, 0);                   /* But SHA-1 supports 64 bit lengths */
+    sha1_add_uncounted(s, 0);                   /* So zero pad the top bits */
+    sha1_add_uncounted(s, s->byte_count >> 29); /* Shifting to multiply by 8 */
+    sha1_add_uncounted(s, s->byte_count >> 21); /* as SHA-1 supports bitstreams as well as */
+    sha1_add_uncounted(s, s->byte_count >> 13); /* byte. */
+    sha1_add_uncounted(s, s->byte_count >> 5);
+    sha1_add_uncounted(s, s->byte_count << 3);
 }
 
 void sha1_final(sha1_context *ctx, void *digest)
 {
-    // TBD
-    (void)ctx;
-    (void)digest;
+    /* Pad to complete the last block */
+    sha1_pad(ctx);
+
+    /* Swap byte order back */
+    for (int i = 0; i < 5; i++) {
+        ctx->state[i] =
+            (((ctx->state[i]) << 24) & 0xff000000)
+            | (((ctx->state[i]) << 8) & 0x00ff0000)
+            | (((ctx->state[i]) >> 8) & 0x0000ff00)
+            | (((ctx->state[i]) >> 24) & 0x000000ff);
+    }
+
+    /* Copy the content of the hash (20 characters) */
+    memcpy(digest, ctx->state, 20);
 }
 
 void sha1(void *digest, const void *data, size_t len)
 {
-    // TBD
-    (void)digest;
-    (void)data;
-    (void)len;
+    sha1_context ctx;
+
+    sha1_init(&ctx);
+    sha1_update(&ctx, (unsigned char *) data, len);
+    sha1_final(&ctx, digest);
 }
+
+#define HMAC_IPAD 0x36
+#define HMAC_OPAD 0x5c
 
 void sha1_init_hmac(sha1_context *ctx, const void *key, size_t key_length)
 {
-    // TBD
-    (void)ctx;
-    (void)key;
-    (void)key_length;
+    uint8_t i;
+    const uint8_t *k = key;
+
+    memset(ctx->key_buffer, 0, SHA1_BLOCK_LENGTH);
+    if (key_length > SHA1_BLOCK_LENGTH) {
+        /* Hash long keys */
+        sha1_init(ctx);
+        while (key_length--) {
+            sha1_update_byte(ctx, *k++);
+        }
+        sha1_final(ctx, ctx->key_buffer);
+    }
+    else {
+        /* Block length keys are used as is */
+        memcpy(ctx->key_buffer, key, key_length);
+    }
+    /* Start inner hash */
+    sha1_init(ctx);
+    for (i = 0; i < SHA1_BLOCK_LENGTH; i++) {
+        sha1_update_byte(ctx, ctx->key_buffer[i] ^ HMAC_IPAD);
+    }
 }
 
 void sha1_final_hmac(sha1_context *ctx, void *digest)
 {
-    // TBD
-    (void)ctx;
-    (void)digest;
+    uint8_t i;
+
+    /* Complete inner hash */
+    sha1_final(ctx, ctx->inner_hash);
+    /* Calculate outer hash */
+    sha1_init(ctx);
+    for (i = 0; i < SHA1_BLOCK_LENGTH; i++) {
+        sha1_update_byte(ctx, ctx->key_buffer[i] ^ HMAC_OPAD);
+    }
+    for (i = 0; i < SHA1_DIGEST_LENGTH; i++) {
+        sha1_update_byte(ctx, ctx->inner_hash[i]);
+    }
+
+    sha1_final(ctx, digest);
 }
--- examples/hello-world/main.c
+++ examples/hello-world/main.c
@@ -20,16 +20,59 @@
  */
 
 #include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+
 #include "hashes/sha1.h"
+#include "xtimer.h"
+#include "periph/gpio.h"
+
+uint8_t result[SHA1_DIGEST_LENGTH];
+char teststring[] = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue";
+uint8_t expected_result[] = { 0x6a, 0x7c, 0x17, 0x10, 0x4e, 0x56, 0x13, 0xa6, 0x82, 0xc5, 0x2b, 0x84, 0x54, 0xae, 0x5e, 0xbc, 0x9e, 0x8a, 0xf1, 0x67 };
+
+size_t teststring_size = (sizeof(teststring)-1);
+
+uint32_t start, stop, t_diff;
 
 int main(void)
 {
-    puts("Hello World!");
+    gpio_init(GPIO_PIN(2,5), GPIO_OUT);
+    sha1_context ctx;
 
+    puts("Hello World!");
     printf("You are running RIOT on a(n) %s board.\n", RIOT_BOARD);
     printf("This board features a(n) %s MCU.\n", RIOT_MCU);
-    sha1_context ctx;
+
+    gpio_set(GPIO_PIN(2,5));
     sha1_init(&ctx);
+    gpio_clear(GPIO_PIN(2,5));
+
+    start = xtimer_now_usec();
+    sha1_update(&ctx, (unsigned char*)teststring, teststring_size);
+    stop = xtimer_now_usec();
+    t_diff = stop - start;
+    printf("Sha1 Update Time: %ld us\n", t_diff);
+
+    gpio_set(GPIO_PIN(2,5));
+    start = xtimer_now_usec();
+    sha1_final(&ctx, result);
+    stop = xtimer_now_usec();
+    t_diff = stop - start;
+    printf("Sha1 Final Time: %ld us\n", t_diff);
+
+    gpio_clear(GPIO_PIN(2,5));
+    if (memcmp(result, expected_result, SHA1_DIGEST_LENGTH) != 0) {
+        printf("SHA-1 Failure\n");
+
+        for (int i = 0; i < SHA1_DIGEST_LENGTH; i++) {
+            printf("%02x ", result[i]);
+        }
+        printf("\n");
+    }
+    else {
+        printf("SHA-1 Success\n");
+    }
 
     return 0;
 }
