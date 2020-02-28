0c5c7c0acb334b3cbf202d2945d1353320bdb43e
Lena Boeckmann <einhornhool@mobi31.inet.haw-hamburg.de>
2020-02-28 10:07:13 +0100

    examples/hello-world: add sha test

--- examples/hello-world/main.c
+++ examples/hello-world/main.c
@@ -20,7 +20,15 @@
  */
 
 #include <stdio.h>
+#include <stdint.h>
+#include <string.h>
 #include "hashes/sha1.h"
+
+uint8_t result[SHA1_DIGEST_LENGTH];
+char teststring[] = "Im dichten Fichtendickicht picken zwei Finken tuechtig";
+uint8_t expected_result[] = { 0xb7, 0xfc, 0x27, 0x42, 0x78, 0x62, 0xae, 0x3b, 0x07, 0x4e, 0x90, 0x21, 0xec, 0x78, 0x92, 0xec, 0x12, 0xac, 0x5e, 0x05 };
+
+size_t teststring_size = (sizeof(teststring)-1);
 
 int main(void)
 {
@@ -30,6 +38,20 @@ int main(void)
     printf("This board features a(n) %s MCU.\n", RIOT_MCU);
     sha1_context ctx;
     sha1_init(&ctx);
+    sha1_update(&ctx, teststring, teststring_size);
+    sha1_final(&ctx, result);
+
+    if (memcmp(result, expected_result, SHA1_DIGEST_LENGTH) != 0) {
+        printf("SHA-1 Failure\n");
+
+        for (int i = 0; i < SHA1_DIGEST_LENGTH; i++) {
+            printf("%02x ", result[i]);
+        }
+        printf("\n");
+    }
+    else {
+        printf("SHA-1 Success\n");
+    }
 
     return 0;
 }
