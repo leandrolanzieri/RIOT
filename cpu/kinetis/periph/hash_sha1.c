bd9806e7c7d2e61988e775e35f9f9fb418ea56ef
Lena Boeckmann <einhornhool@mobi31.inet.haw-hamburg.de>
2020-02-28 10:07:48 +0100

    cpu/kinetis: add sha1 hwa implementation

--- cpu/kinetis/periph/hash_sha1.c
+++ cpu/kinetis/periph/hash_sha1.c
@@ -17,48 +17,126 @@
  *
  * @file
  * @brief       Implementation of the SHA-1 hashing function
+ *              https://github.com/linwenjian/Quadcopter_Will/blob/fd0b0d3db28ff4d2b93b3c481a0378bd0302ae57/Software/SDK1_1/platform/drivers/src/mmcau/src/mmcau_sha1_functions.c
  *
  * @author      Wei Dai and others
- * @author      Oliver Hahm <oliver.hahm@inria.fr>
+ * @author      Lena Boeckmann <lena.boeckmann@haw-hamburg.de>
  */
 
 #include <stdint.h>
 #include <string.h>
 
-#include "MKW24D5.h"
+#include "vendor/MKW21D5.h"
 #include "hashes/sha1.h"
+#include "mmcau_hash_sha1.h"
 
 #define ENABLE_DEBUG    (1)
 #include "debug.h"
 
+#define SHA1_K0  0x5a827999
+#define SHA1_K20 0x6ed9eba1
+#define SHA1_K40 0x8f1bbcdc
+#define SHA1_K60 0xca62c1d6
+
 void sha1_init(sha1_context *ctx)
 {
-    // TBD
-    (void)ctx;
     DEBUG("SHA1 init HW accelerated implementation\n");
+    /* Initialize hash variables */
+    ctx->state[0] = 0x67452301;
+    ctx->state[1] = 0xefcdab89;
+    ctx->state[2] = 0x98badcfe;
+    ctx->state[3] = 0x10325476;
+    ctx->state[4] = 0xc3d2e1f0;
+}
+
+static void sha1_step(int count, int i, int constant, int *w)
+{
+    for (int j = 0; j < count; j++) {
+            CAU->DIRECT[0] = MMCAU_2_CMDS((HASH+HFC), (ADRA+CA4));
+            CAU->ADR_CAA = constant;
+            CAU->LDR_CA[5] = w[i-16];
+            CAU->XOR_CA[5] = w[i-14];
+            CAU->XOR_CA[5] = w[i-8];
+            CAU->XOR_CA[5] = w[i-3];
+            CAU->ROTL_CA[5] = 1;
+            w[i++] = CAU->STR_CA[5];
+            CAU->DIRECT[0] = MMCAU_2_CMDS((ADRA+CA5), SHS);
+        }
 }
 
 void sha1_update(sha1_context *ctx, const void *data, size_t len)
 {
-    // TBD
-    (void)ctx;
-    (void)data;
-    (void)len;
+    int num_blks;
+    int k = 0;
+    int j;
+    int w[80];
+
+    unsigned char *da = (unsigned char*) data;
+    /* The code I copied this from gets a number of 512 bit blocks instead of the input length. To be able to just recycle the code I convert len into  num_blks */
+
+    if(len <= 64) {
+        num_blks = 1;
+    }
+    else {
+        num_blks = (len % 64 == 0) ? (len / 64) : ((len / 64) + 1);
+    }
+
+    DEBUG("num_blks: %d\n", num_blks);
+    /* Initialize hash variables in CAU */
+    for (j = 0; j < 5; j++) {
+        CAU->LDR_CA[j] = ctx->state[j];
+        DEBUG("LDR_CA[%d]: %lx\n", j, CAU->STR_CA[j]);
+    }
+
+    for (int n = 0; n < num_blks; n++) {
+        int i = 0;
+
+        CAU->DIRECT[0] = MMCAU_1_CMD((MVRA+CA0));     /* a -> CAA */
+        CAU->ROTL_CAA = 5;                          /* rotate 5 */
+
+        for (j = 0; j < 16; j++, k++) {
+            w[i] = BYTEREV(da[k]);
+            DEBUG("da[%d]: %x\n", i, da[k]);
+            CAU->DIRECT[0] = MMCAU_2_CMDS((HASH+HFC), (ADRA+CA4));
+            CAU->ADR_CAA = SHA1_K0;
+            CAU->ADR_CAA = w[i++];
+            CAU->DIRECT[0] = MMCAU_1_CMD(SHS);
+        }
+
+        sha1_step(4, i, SHA1_K0, w);
+        sha1_step(20, i, SHA1_K20, w);
+        sha1_step(20, i, SHA1_K40, w);
+        sha1_step(20, i, SHA1_K60, w);
+
+        for (j = 0; j < 5; j++) {
+            CAU->ADR_CA[j] = ctx->state[j];
+            ctx->state[j] = CAU->STR_CA[j];
+        }
+    }
 }
 
 void sha1_final(sha1_context *ctx, void *digest)
 {
-    // TBD
-    (void)ctx;
-    (void)digest;
+    // /* Swap byte order back */
+    // for (int i = 0; i < 5; i++) {
+    //     ctx->state[i] =
+    //         (((ctx->state[i]) << 24) & 0xff000000)
+    //         | (((ctx->state[i]) << 8) & 0x00ff0000)
+    //         | (((ctx->state[i]) >> 8) & 0x0000ff00)
+    //         | (((ctx->state[i]) >> 24) & 0x000000ff);
+    // }
+
+    /* Copy the content of the hash (20 characters) */
+    memcpy(digest, ctx->state, 20);
 }
 
 void sha1(void *digest, const void *data, size_t len)
 {
-    // TBD
-    (void)digest;
-    (void)data;
-    (void)len;
+    sha1_context ctx;
+
+    sha1_init(&ctx);
+    sha1_update(&ctx, (unsigned char *) data, len);
+    sha1_final(&ctx, digest);
 }
 
 void sha1_init_hmac(sha1_context *ctx, const void *key, size_t key_length)
