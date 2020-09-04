3655f031552f6584a8e0aa483bab214642cc06c7
Einhornhool <26007369+Einhornhool@users.noreply.github.com>
2020-09-04 22:26:12 +0300

    sys/include/crypto: update ECB function declarations to use 'const'

--- sys/include/crypto/aes.h
+++ sys/include/crypto/aes.h
@@ -142,11 +142,11 @@ int aes_decrypt_cbc(cipher_context_t *context, uint8_t iv[16],
                        const uint8_t *input, size_t length, uint8_t *output);
 
 // TODO document
-int aes_encrypt_ecb(cipher_context_t *context, uint8_t *input,
+int aes_encrypt_ecb(cipher_context_t *context, const uint8_t *input,
                        size_t length, uint8_t *output);
 
 // TODO document
-int aes_decrypt_ecb(cipher_context_t *context, uint8_t *input,
+int aes_decrypt_ecb(cipher_context_t *context, const uint8_t *input,
                        size_t length, uint8_t *output);
 
 #ifdef __cplusplus
