15143b8fc948aec672fbb487f57495702e639ae2
Einhornhool <26007369+Einhornhool@users.noreply.github.com>
2020-09-04 22:31:27 +0300

    examples/crypto-ewsn2020: add define to declare active gpio for nrf52840dk

--- examples/crypto-ewsn2020/main.c
+++ examples/crypto-ewsn2020/main.c
@@ -38,6 +38,10 @@
     gpio_t active_gpio = GPIO_PIN(2,5);
 #endif /* BOARD_PBA_D_01_KW2X */
 
+#ifdef BOARD_NRF52840DK
+    gpio_t active_gpio = GPIO_PIN(1,15);
+#endif /* BOARD_NRF52840DK */
+
 int main(void)
 {
     // sha1_test(active_gpio);
