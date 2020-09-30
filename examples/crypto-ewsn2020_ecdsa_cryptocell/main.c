e4acab0b052f0e87bf7509b6c7d659e62d298335
Einhornhool <26007369+Einhornhool@users.noreply.github.com>
2020-09-30 23:29:06 +0300

    examples/crypto-ewsn2020_ecdh: prepare cryptocell ecdsa for tests

--- examples/crypto-ewsn2020_ecdsa_cryptocell/main.c
+++ examples/crypto-ewsn2020_ecdsa_cryptocell/main.c
@@ -19,22 +19,27 @@
  */
 
 #include <stdio.h>
-#include <assert.h>
-#include <stdlib.h>
-
-#include "armcc_setup.h"
 #include "cryptocell_util.h"
 
 #include "cryptocell_incl/sns_silib.h"
 #include "cryptocell_incl/crys_ecpki_build.h"
 #include "cryptocell_incl/crys_ecpki_ecdsa.h"
-#include "cryptocell_incl/crys_ecpki_dh.h"
+// #include "cryptocell_incl/crys_ecpki_dh.h"
 #include "cryptocell_incl/crys_ecpki_kg.h"
 #include "cryptocell_incl/crys_ecpki_domain.h"
 
+#ifndef COSY_TEST
+#include "xtimer.h"
+#include "ps.h"
 #include "periph/gpio.h"
 
 gpio_t active_gpio = GPIO_PIN(1, 7);
+
+#define ITERATIONS                  (50)
+#endif
+
+#define SHA256_DIGEST_SIZE          (32)
+#define ECDSA_MESSAGE_SIZE          (127)
 
 extern CRYS_RND_State_t*     rndState_ptr;
 extern CRYS_RND_WorkBuff_t*  rndWorkBuff_ptr;
@@ -45,6 +50,9 @@ CRYS_ECDH_TempData_t signOutBuff;
 CRYS_ECDSA_SignUserContext_t SignUserContext;
 CRYS_ECDSA_VerifyUserContext_t VerifyUserContext;
 CRYS_ECPKI_Domain_t* pDomain;
+CRYS_ECDH_TempData_t* TempDHBuffptr;
+CRYS_ECPKI_KG_TempData_t* TempECCKGBuffptr;
+CRYS_ECDH_TempData_t TempDHBuff;
 CRYS_ECPKI_KG_TempData_t TempECCKGBuff;
 CRYS_ECPKI_KG_FipsContext_t FipsBuff;
 SaSiRndGenerateVectWorkFunc_t rndGenerateVectFunc;
@@ -53,19 +61,19 @@ uint32_t ecdsa_sig_size = 64;
 void _init_vars(void)
 {
     rndGenerateVectFunc = CRYS_RND_GenerateVector;
-    pDomain = (CRYS_ECPKI_Domain_t*)CRYS_ECPKI_GetEcDomain(CRYS_ECPKI_DomainID_secp224r1);
+    pDomain = (CRYS_ECPKI_Domain_t*)CRYS_ECPKI_GetEcDomain(CRYS_ECPKI_DomainID_secp256r1);
 }
 
 
 void _gen_keypair(void)
 {
+#ifndef COSY_TEST
     int ret = 0;
 
     cryptocell_enable();
 
     gpio_set(active_gpio);
-    ret = CRYS_ECPKI_GenKeyPair (rndState_ptr, rndGenerateVectFunc, pDomain, &UserPrivKey,
-    &UserPublKey, &TempECCKGBuff, &FipsBuff);
+    ret = CRYS_ECPKI_GenKeyPair(rndState_ptr, rndGenerateVectFunc, pDomain, &UserPrivKey, &UserPublKey, &TempECCKGBuff, &FipsBuff);
     gpio_clear(active_gpio);
 
     cryptocell_disable();
@@ -74,20 +82,25 @@ void _gen_keypair(void)
         printf("CRYS_ECPKI_GenKeyPair for key pair 1 failed with 0x%x \n",ret);
         return;
     }
+#else
+    cryptocell_enable();
+    CRYS_ECPKI_GenKeyPair(rndState_ptr, rndGenerateVectFunc, pDomain, &UserPrivKey, &UserPublKey, &TempECCKGBuff, &FipsBuff);
+    cryptocell_disable();
+#endif
 }
 
 void _sign_verify(void)
 {
-    int ret = 0;
-    // this is a test message copied from relic tests
-    uint8_t m[5] = { 0, 1, 2, 3, 4 };
+    uint8_t msg[ECDSA_MESSAGE_SIZE] = { 0x0b };
 
+#ifndef COSY_TEST
+    int ret = 0;
     /*Call CRYS_ECDSA_Sign to create signature from input buffer using created private key*/
     cryptocell_enable();
 
     gpio_set(active_gpio);
-    ret = CRYS_ECDSA_Sign (rndState_ptr, rndGenerateVectFunc, &SignUserContext,
-    &UserPrivKey, CRYS_ECPKI_HASH_SHA256_mode, m, sizeof(m), (uint8_t*)&signOutBuff, &ecdsa_sig_size);
+    ret = CRYS_ECDSA_Sign (rndState_ptr, rndGenerateVectFunc,
+    &SignUserContext, &UserPrivKey, CRYS_ECPKI_HASH_SHA256_mode, msg, sizeof(msg), (uint8_t*)&signOutBuff, &ecdsa_sig_size);
     gpio_clear(active_gpio);
 
     cryptocell_disable();
@@ -100,33 +113,56 @@ void _sign_verify(void)
     cryptocell_enable();
 
     gpio_set(active_gpio);
-    ret =  CRYS_ECDSA_Verify (&VerifyUserContext, &UserPublKey, CRYS_ECPKI_HASH_SHA256_mode,
-    (uint8_t*)&signOutBuff, ecdsa_sig_size, m, sizeof(m));
+    ret =  CRYS_ECDSA_Verify (&VerifyUserContext, &UserPublKey, CRYS_ECPKI_HASH_SHA256_mode, (uint8_t*)&signOutBuff, ecdsa_sig_size, msg, sizeof(msg));
     gpio_clear(active_gpio);
 
     cryptocell_disable();
-
     if (ret != SA_SILIB_RET_OK){
         printf("CRYS_ECDSA_Verify failed with 0x%x \n",ret);
         return;
     }
     puts("VALID\n");
+#else
+    cryptocell_enable();
+    CRYS_ECDSA_Sign (rndState_ptr, rndGenerateVectFunc,
+    &SignUserContext, &UserPrivKey, CRYS_ECPKI_HASH_SHA256_mode, msg, sizeof(msg), (uint8_t*)&signOutBuff, &ecdsa_sig_size);
+    cryptocell_disable();
+
+    cryptocell_enable();
+    CRYS_ECDSA_Verify (&VerifyUserContext, &UserPublKey, CRYS_ECPKI_HASH_SHA256_mode, (uint8_t*)&signOutBuff, ecdsa_sig_size, msg, sizeof(msg));
+    cryptocell_disable();
+#endif
 }
 
 int main(void)
 {
+#ifndef COSY_TEST
     puts("'crypto-ewsn2020_ecdsa cryptocell'");
     gpio_init(active_gpio, GPIO_OUT);
     gpio_clear(active_gpio);
-    // init memory
+
+    xtimer_sleep(1);
+
+    for (int i = 0; i < ITERATIONS; i++) {
+        gpio_set(active_gpio);
+        _init_vars();
+        gpio_clear(active_gpio);
+#else
     _init_vars();
+#endif
+        // generate keypairs
+        _gen_keypair();
 
-    // generate keypairs
-    _gen_keypair();
+        // sign data and verify with public ley
+        _sign_verify();
 
-    // sign data and verify with public ley
-    _sign_verify();
-
+#ifndef CORY_TEST
+    }
+    ps();
+    printf("sizeof(UserPrivKey): %i\n", sizeof(UserPrivKey));
+    printf("sizeof(UserPubKey): %i\n", sizeof(UserPublKey));
+#endif
+    puts("DONE");
     return 0;
 }
 
