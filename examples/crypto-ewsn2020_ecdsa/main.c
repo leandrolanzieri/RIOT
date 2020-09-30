14170759b8e100960bfc8dba6c20ae69872968ac
Einhornhool <26007369+Einhornhool@users.noreply.github.com>
2020-09-30 23:28:19 +0300

    examples/crypto-ewsn2020_ecdh: prepare relic ecdsa for tests

--- examples/crypto-ewsn2020_ecdsa/main.c
+++ examples/crypto-ewsn2020_ecdsa/main.c
@@ -18,11 +18,23 @@
  * @}
  */
 
+#ifndef COSY_TEST
 #include <stdio.h>
-#include <assert.h>
-#include <stdlib.h>
+#include "periph/gpio.h"
+#include "xtimer.h"
+#include "ps.h"
+
+gpio_t active_gpio = GPIO_PIN(1, 7);
+#endif
 
 #include "relic.h"
+
+#ifndef COSY_TEST
+#define ITERATIONS          (50)
+#endif
+
+#define SHA256_DIGEST_SIZE  (32)
+#define ECDSA_MESSAGE_SIZE  (127)
 
 typedef struct
 {
@@ -34,6 +46,7 @@ static key_struct_t keyA;
 
 void _init_mem(key_struct_t *key)
 {
+    ep_param_set(NIST_P256);
     // set up memory
     bn_null(key->priv);
     ec_null(key->pub);
@@ -44,55 +57,91 @@ void _init_mem(key_struct_t *key)
 
 void _gen_keypair(void)
 {
+#ifndef COSY_TEST
     // generate pubkey pair A
+    gpio_set(active_gpio);
     int ret = cp_ecdsa_gen(keyA.priv, keyA.pub);
+    gpio_clear(active_gpio);
     if (ret == STS_ERR) {
-        printf("ECDH key pair creation failed. Not good :( \n");
+        puts("ECDH key pair creation failed. Not good :( \n");
         return;
     }
+#else
+    cp_ecdsa_gen(keyA.priv, keyA.pub);
+#endif
 }
 
 void _sign_verify(void)
 {
-    // this is a test message copied from relic tests
-    uint8_t m[5] = { 0, 1, 2, 3, 4 };
+    uint8_t msg[ECDSA_MESSAGE_SIZE] = { 0x0b };
+    uint8_t hash[SHA256_DIGEST_SIZE];
 
     bn_t r, s;
     bn_new(r);
     bn_new(s);
 
+#ifndef COSY_TEST
+    int ret;
+    gpio_set(active_gpio);
+    md_map_sh256(hash, msg, ECDSA_MESSAGE_SIZE);
+    gpio_clear(active_gpio);
+
     // "0" selects message format without hashing. "1" would indicatehashing is needed
-    cp_ecdsa_sig(r, s, m, sizeof(m), 0, keyA.priv);
+    gpio_set(active_gpio);
+    cp_ecdsa_sig(r, s, hash, sizeof(hash), 0, keyA.priv);
+    gpio_clear(active_gpio);
 
     // verify
-    if (cp_ecdsa_ver(r, s, m, sizeof(m), 0, keyA.pub) == 1)
-    {
+    gpio_set(active_gpio);
+    ret = cp_ecdsa_ver(r, s, hash, sizeof(hash), 0, keyA.pub);
+    gpio_clear(active_gpio);
+
+    if (ret == 1) {
         puts("VALID");
     }
-    else
-    {
+    else {
         puts("INVALID");
     }
+#else
+    md_map_sh256(hash, msg, ECDSA_MESSAGE_SIZE);
+    cp_ecdsa_sig(r, s, hash, sizeof(hash), 0, keyA.priv);
+    cp_ecdsa_ver(r, s, hash, sizeof(hash), 0, keyA.pub);
+#endif
 }
 
 int main(void)
 {
-    puts("'crypto-ewsn2020_ecdsa'");
     core_init();
 
-    // set NIST P-256 elliptic curve
-    ep_param_set(NIST_P256);
-    ec_param_print();
+#ifndef COSY_TEST
+    puts("'crypto-ewsn2020_ecdsa'");
+    gpio_init(active_gpio, GPIO_OUT);
+    gpio_clear(active_gpio);
 
-    // init memory
-    _init_mem(&keyA);
+    xtimer_sleep(1);
 
-    // generate keypairs
-    _gen_keypair();
+    for (int i = 0; i < ITERATIONS; i++){
 
-    // sign data and verify with public ley
-    _sign_verify();
+        gpio_set(active_gpio);
+        _init_mem(&keyA);
+        gpio_clear(active_gpio);
+#else
+        _init_mem(&keyA);
+#endif
+        // generate keypairs
+        _gen_keypair();
+
+        // sign data and verify with public ley
+        _sign_verify();
+
+#ifndef COSY_TEST
+    }
+
+    ps();
+    printf("sizeof(keyA): %i\n", sizeof(keyA));
+#endif
 
     core_clean();
+    puts("DONE");
     return 0;
 }
