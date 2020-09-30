7dbb577fbc9412276523db0c54625fab16829805
Einhornhool <26007369+Einhornhool@users.noreply.github.com>
2020-09-30 23:28:52 +0300

    examples/crypto-ewsn2020_ecdh: prepare cryptocell ecdh for tests

--- examples/crypto-ewsn2020_ecdh_cryptocell/main.c
+++ examples/crypto-ewsn2020_ecdh_cryptocell/main.c
@@ -19,36 +19,34 @@
  */
 
 #include <stdio.h>
-#include <assert.h>
-#include <stdlib.h>
-#include <string.h>
-
-#include "armcc_setup.h"
 #include "cryptocell_util.h"
 
 #include "cryptocell_incl/sns_silib.h"
 #include "cryptocell_incl/crys_ecpki_build.h"
-#include "cryptocell_incl/crys_ecpki_ecdsa.h"
+// #include "cryptocell_incl/crys_ecpki_ecdsa.h"
 #include "cryptocell_incl/crys_ecpki_dh.h"
 #include "cryptocell_incl/crys_ecpki_kg.h"
 #include "cryptocell_incl/crys_ecpki_domain.h"
 
+#ifndef COSY_TEST
+#include <stdio.h>
+#include <string.h>
 #include "periph/gpio.h"
+#include "xtimer.h"
+#include "ps.h"
 
 gpio_t active_gpio = GPIO_PIN(1, 7);
 
-#define SHARED_SECRET_MAX_LENGHT         (250)
+#define ITERATIONS                  (50)
+#endif
+
+#define SHARED_SECRET_MAX_LENGHT    (250)
 
 extern CRYS_RND_State_t*     rndState_ptr;
 extern CRYS_RND_WorkBuff_t*  rndWorkBuff_ptr;
 
 CRYS_ECPKI_UserPrivKey_t UserPrivKey1;
 CRYS_ECPKI_UserPublKey_t UserPublKey1;
-CRYS_ECPKI_UserPrivKey_t UserPrivKey2;
-CRYS_ECPKI_UserPublKey_t UserPublKey2;
-CRYS_ECDH_TempData_t signOutBuff;
-CRYS_ECDSA_SignUserContext_t SignUserContext;
-CRYS_ECDSA_VerifyUserContext_t VerifyUserContext;
 CRYS_ECPKI_Domain_t* pDomain;
 CRYS_ECDH_TempData_t* TempDHBuffptr;
 CRYS_ECPKI_KG_TempData_t* TempECCKGBuffptr;
@@ -56,11 +54,15 @@ CRYS_ECDH_TempData_t TempDHBuff;
 CRYS_ECPKI_KG_TempData_t TempECCKGBuff;
 CRYS_ECPKI_KG_FipsContext_t FipsBuff;
 SaSiRndGenerateVectWorkFunc_t rndGenerateVectFunc;
-uint32_t ecdsa_sig_size = 64;
 uint8_t sharedSecret1ptr[SHARED_SECRET_MAX_LENGHT];
-uint8_t sharedSecret2ptr[SHARED_SECRET_MAX_LENGHT];
 uint32_t sharedSecret1Size = SHARED_SECRET_MAX_LENGHT;
+
+#ifndef COSY_TEST
+CRYS_ECPKI_UserPrivKey_t UserPrivKey2;
+CRYS_ECPKI_UserPublKey_t UserPublKey2;
+uint8_t sharedSecret2ptr[SHARED_SECRET_MAX_LENGHT];
 uint32_t sharedSecret2Size = SHARED_SECRET_MAX_LENGHT;
+#endif
 
 void _init_vars(void)
 {
@@ -72,47 +74,42 @@ void _init_vars(void)
 
 void _gen_keypair(void)
 {
+#ifndef COSY_TEST
     int ret = 0;
 
     cryptocell_enable();
-
     gpio_set(active_gpio);
-    ret = CRYS_ECPKI_GenKeyPair (rndState_ptr, rndGenerateVectFunc, pDomain, &UserPrivKey1,
-    &UserPublKey1, TempECCKGBuffptr, &FipsBuff);
+    ret = CRYS_ECPKI_GenKeyPair (rndState_ptr, rndGenerateVectFunc, pDomain, &UserPrivKey1, &UserPublKey1, TempECCKGBuffptr, &FipsBuff);
     gpio_clear(active_gpio);
-
     cryptocell_disable();
-
     if (ret != SA_SILIB_RET_OK){
         printf("CRYS_ECPKI_GenKeyPair for key pair 1 failed with 0x%x \n",ret);
         return;
     }
-
     cryptocell_enable();
-
-    gpio_set(active_gpio);
-    ret = CRYS_ECPKI_GenKeyPair (rndState_ptr, rndGenerateVectFunc, pDomain, &UserPrivKey2,
-    &UserPublKey2, TempECCKGBuffptr, &FipsBuff);
-    gpio_clear(active_gpio);
-
+    ret = CRYS_ECPKI_GenKeyPair (rndState_ptr, rndGenerateVectFunc, pDomain, &UserPrivKey2, &UserPublKey2, TempECCKGBuffptr, &FipsBuff);
     cryptocell_disable();
-
     if (ret != SA_SILIB_RET_OK){
         printf("CRYS_ECPKI_GenKeyPair for key pair 2 failed with 0x%x \n",ret);
         return;
     }
+#else
+    cryptocell_enable();
+    CRYS_ECPKI_GenKeyPair (rndState_ptr, rndGenerateVectFunc, pDomain, &UserPrivKey1, &UserPublKey1, TempECCKGBuffptr, &FipsBuff);
+    cryptocell_disable();
+#endif
 }
 
 void _derive_shared_secret(void)
 {
+#ifndef COSY_TEST
     int ret = 0;
-
     /* Generating the Secret for user 1*/
     /*---------------------------------*/
     cryptocell_enable();
 
     gpio_set(active_gpio);
-    ret = CRYS_ECDH_SVDP_DH(&UserPublKey2, &UserPrivKey1,sharedSecret1ptr, &sharedSecret1Size, TempDHBuffptr);
+    ret = CRYS_ECDH_SVDP_DH(&UserPublKey2, &UserPrivKey1, sharedSecret1ptr, &sharedSecret1Size, TempDHBuffptr);
     gpio_clear(active_gpio);
 
     cryptocell_disable();
@@ -125,18 +122,13 @@ void _derive_shared_secret(void)
     /* Generating the Secret for user 2*/
     /*---------------------------------*/
     cryptocell_enable();
-
-    gpio_set(active_gpio);
     ret = CRYS_ECDH_SVDP_DH(&UserPublKey1, &UserPrivKey2, sharedSecret2ptr, &sharedSecret2Size, TempDHBuffptr);
-    gpio_clear(active_gpio);
-
     cryptocell_disable();
 
     if (ret != SA_SILIB_RET_OK){
         printf(" CRYS_ECDH_SVDP_DH for secret 2 failed with 0x%x \n",ret);
         return;
     }
-
     // generated secret should be the same on both
     if (memcmp(sharedSecret1ptr, sharedSecret2ptr, SHARED_SECRET_MAX_LENGHT)) {
         puts("ERROR");
@@ -144,20 +136,42 @@ void _derive_shared_secret(void)
     else {
         puts("SUCCESS");
     }
+#else
+    cryptocell_enable();
+    CRYS_ECDH_SVDP_DH(&UserPublKey1, &UserPrivKey1, sharedSecret1ptr, &sharedSecret1Size, TempDHBuffptr);
+    cryptocell_disable();
+#endif
 }
 
 int main(void)
 {
+#ifndef COSY_TEST
     puts("'crypto-ewsn2020_ecdh_cryptocell'");
     gpio_init(active_gpio, GPIO_OUT);
     gpio_clear(active_gpio);
-    _init_vars();
+    xtimer_sleep(1);
 
-    // generate two instances of keypairs
-    _gen_keypair();
+    for (int i = 0; i < ITERATIONS; i++) {
+        gpio_set(active_gpio);
+        _init_vars();
+        gpio_clear(active_gpio);
+#else
+        _init_vars();
+#endif
+        // generate two instances of keypairs
+        _gen_keypair();
 
-    // derive and compare secrets generated on both
-    _derive_shared_secret();
+        // derive and compare secrets generated on both
+        _derive_shared_secret();
+#ifndef COSY_TEST
+    }
 
+    ps();
+    printf("sizeof(UserPrivKey1): %i\n", sizeof(UserPrivKey1));
+    printf("sizeof(UserPubKey1): %i\n", sizeof(UserPublKey1));
+    printf("sizeof(UserPrivKey2): %i\n", sizeof(UserPrivKey2));
+    printf("sizeof(UserPubKey2): %i\n", sizeof(UserPublKey2));
+#endif
+    puts("DONE");
     return 0;
 }
