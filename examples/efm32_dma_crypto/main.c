703dcd8294f0978834f0661a58efc86677299151
Leandro Lanzieri <leandro.lanzieri@haw-hamburg.de>
2020-10-06 15:55:01 +0200

    examples/efm_dma: add configuration for number of bytes

--- examples/efm32_dma_crypto/main.c
+++ examples/efm32_dma_crypto/main.c
@@ -39,26 +39,34 @@ static uint8_t key_0[] = {
     0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7,
     0x8, 0x9, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF
 };
-static uint8_t plain_0[] = {
+
+#if IS_ACTIVE(CONFIG_AES_NUMBYTES_16)
+static uint8_t plainText[] = {
     0x8, 0x9, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF,
     0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7
 };
-// static uint8_t plain_1[] = {
-//     0x8, 0x9, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF,
-//     0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7,
-//     0x8, 0x9, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF,
-//     0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7
-// };
-static uint8_t cipher_0[] = {
+static uint8_t cipherText[] = {
     0x37, 0x29, 0xa3, 0x6c, 0xaf, 0xe9, 0x84, 0xff,
     0x46, 0x22, 0x70, 0x42, 0xee, 0x24, 0x83, 0xf6
 };
-// static uint8_t cipher_1[] = {
-//     0x37, 0x29, 0xa3, 0x6c, 0xaf, 0xe9, 0x84, 0xff,
-//     0x46, 0x22, 0x70, 0x42, 0xee, 0x24, 0x83, 0xf6,
-//     0x37, 0x29, 0xa3, 0x6c, 0xaf, 0xe9, 0x84, 0xff,
-//     0x46, 0x22, 0x70, 0x42, 0xee, 0x24, 0x83, 0xf6
-// };
+#define AES_NUMBYTES (16UL)
+#else
+static uint8_t plainText[] = {
+    0x8, 0x9, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF,
+    0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7,
+    0x8, 0x9, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF,
+    0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7
+};
+static uint8_t cipherText[] = {
+    0x37, 0x29, 0xa3, 0x6c, 0xaf, 0xe9, 0x84, 0xff,
+    0x46, 0x22, 0x70, 0x42, 0xee, 0x24, 0x83, 0xf6,
+    0x37, 0x29, 0xa3, 0x6c, 0xaf, 0xe9, 0x84, 0xff,
+    0x46, 0x22, 0x70, 0x42, 0xee, 0x24, 0x83, 0xf6
+};
+#define AES_NUMBYTES (32UL)
+#endif
+
+static uint8_t cipherData[AES_NUMBYTES];
 
 int is_result_expected(uint8_t *result, uint8_t *expected, size_t bytes)
 {
@@ -69,8 +77,6 @@ int is_result_expected(uint8_t *result, uint8_t *expected, size_t bytes)
     }
     return 1;
 }
-
-static uint8_t cipherData[AES_SIZE_BYTES];
 
 static inline uint32_t get_dma_source(CRYPTO_TypeDef *dev)
 {
@@ -111,8 +117,6 @@ static inline uint32_t get_dma_source_and_signal_data0_write(CRYPTO_TypeDef *dev
         return LDMA_CH_REQSEL_SIGSEL_CRYPTO1DATA0WR | LDMA_CH_REQSEL_SOURCESEL_CRYPTO1;
     }
 }
-
-static mutex_t dma_lock;
 
 static void init_crypto_read_dma_channel(crypto_device_t* crypto, uint8_t *out)
 {
@@ -156,14 +160,15 @@ static void init_crypto_write_dma_channel(crypto_device_t *crypto, uint8_t *in)
     LDMA->CH[crypto->write_dma_ch].DST = (uint32_t)&(crypto->dev->DATA0BYTE); /* set destination address to the DATA0 register of the used CRYPTO device */
 }
 
-static void init_crypto_aes_128_encrypt(crypto_device_t* crypto)
+static void init_crypto_aes_128_encrypt(crypto_device_t* crypto, size_t length)
 {
     /* configure the CRYPTO device */
     crypto->dev->CTRL =   CRYPTO_CTRL_DMA0RSEL_DATA0 /* use DATA0 register for DMA0RD DMA requests */
                         | CRYPTO_CTRL_DMA0MODE_FULLBYTE  /* read the full target register on every DMA transaction */
                         | CRYPTO_CTRL_AES_AES128;    /* use AES-128 mode */
 
-    crypto->dev->SEQCTRL |= 16; /* NOTE: we want to handle 16 bytes */
+    crypto->dev->WAC = 0;
+    crypto->dev->SEQCTRL= length; /* NOTE: we want to handle 16 bytes in total */
 
     /* load a sequence of commands */
     crypto->dev->IEN |= CRYPTO_IEN_SEQDONE;
@@ -179,6 +184,7 @@ int main(void)
     printf("You are running RIOT on a(n) %s board.\n", RIOT_BOARD);
     printf("This board features a(n) %s MCU.\n", RIOT_MCU);
     puts("=== Test AES on CRYPTO ===");
+    printf("-> Number of bytes: %ld\n", AES_NUMBYTES);
 
     crypto_device_t *crypto = crypto_acquire_dev();
 
@@ -195,18 +201,17 @@ int main(void)
     if (IS_ACTIVE(CONFIG_AES_WITH_DMA)) {
         puts("== Testing AES with DMA ==");
 
-        mutex_init(&dma_lock);
-        mutex_lock(&dma_lock);
-
         LDMA_Init_t ldma_init = LDMA_INIT_DEFAULT;
         LDMA_Init(&ldma_init);
 
         init_crypto_read_dma_channel(crypto, cipherData);
-        init_crypto_write_dma_channel(crypto, plain_0);
-        init_crypto_aes_128_encrypt(crypto);
+        init_crypto_write_dma_channel(crypto, plainText);
+        init_crypto_aes_128_encrypt(crypto, AES_NUMBYTES);
 
         /* wait for instruction, sequence and DMA completion */
-        while ((crypto->dev->STATUS &(CRYPTO_STATUS_INSTRRUNNING | CRYPTO_STATUS_SEQRUNNING | CRYPTO_STATUS_DMAACTIVE)) != 0) { }
+        while ((crypto->dev->STATUS &(CRYPTO_STATUS_INSTRRUNNING
+                                    | CRYPTO_STATUS_SEQRUNNING
+                                    | CRYPTO_STATUS_DMAACTIVE)) != 0) { }
     }
     else {
         puts("== Testing AES without DMA ==");
@@ -214,7 +219,7 @@ int main(void)
 
         /* load the plain text in the CRYPTO */
         for (int i = 0; i < 16; i++) {
-            crypto->dev->DATA0BYTE = plain_0[i];
+            crypto->dev->DATA0BYTE = plainText[i];
         }
 
         crypto->dev->CTRL = CRYPTO_CTRL_AES_AES128; /* use AES-128 mode */
@@ -226,21 +231,22 @@ int main(void)
         while ((crypto->dev->IF & CRYPTO_IF_INSTRDONE) == 0UL) { }
         crypto->dev->IFC = CRYPTO_IF_INSTRDONE;
 
-        for (unsigned i = 0; i < AES_SIZE_BYTES; i++) {
+        // TODO: Here goes a loop!
+        for (unsigned i = 0; i < AES_NUMBYTES; i++) {
             cipherData[i] = crypto->dev->DATA0BYTE;
         }
     }
 
     puts("-> Done");
 
-    if (is_result_expected(cipherData, cipher_0, AES_SIZE_BYTES)) {
+    if (is_result_expected(cipherData, cipherText, AES_NUMBYTES)) {
             puts("=> CORRECT");
     }
     else {
         puts("=> INCORRECT!");
     }
 
-    for (unsigned i = 0; i < AES_SIZE_BYTES; i++) {
+    for (unsigned i = 0; i < AES_NUMBYTES; i++) {
             printf("[0x%x], ", cipherData[i]);
     }
 
