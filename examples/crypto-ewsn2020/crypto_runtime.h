3559104b02f108b831499b84118c4d67eb4107ee
Einhornhool <26007369+Einhornhool@users.noreply.github.com>
2020-10-26 21:30:52 +0200

    examples/crypto_ewsn2020: Add sync pins for other boards

--- examples/crypto-ewsn2020/crypto_runtime.h
+++ examples/crypto-ewsn2020/crypto_runtime.h
@@ -20,12 +20,8 @@ void aes_ecb_dec_test_energy(gpio_t, gpio_t);
 #ifdef BOARD_PBA_D_01_KW2X
     gpio_t active_gpio = GPIO_PIN(2, 5);
     gpio_t gpio_aes_key = GPIO_PIN(2, 6);
+    gpio_t gpio_sync_pin = GPIO_PIN(2, 7);
 #endif /* BOARD_PBA_D_01_KW2X */
-
-#ifdef BOARD_FRDM_K64F
-    gpio_t active_gpio = GPIO_PIN(3, 1);
-    gpio_t gpio_aes_key = GPIO_PIN(3, 3);
-#endif /* BOARD_FRDM_K64F */
 
 #ifdef BOARD_NRF52840DK
     gpio_t active_gpio = GPIO_PIN(1, 7);
@@ -36,6 +32,7 @@ void aes_ecb_dec_test_energy(gpio_t, gpio_t);
 #ifdef BOARD_SLSTK3402A
     gpio_t active_gpio = GPIO_PIN(0, 6);
     gpio_t gpio_aes_key = GPIO_PIN(0, 7);
+    gpio_t gpio_sync_pin = GPIO_PIN(0, 8);
 #endif /* BOARD_SLSTK3402A */
 
 #endif /* CRYPTO_RUNTIME_H */
