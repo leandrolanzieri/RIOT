1a93b66716671324cde29d949b1badb8078666db
Leandro Lanzieri <leandro.lanzieri@haw-hamburg.de>
2021-12-06 09:58:36 +0100

    cpu/nrf9160: add cryptocell initialization

---
+++ cpu/nrf9160/include/cc310.h
@@ -0,0 +1,9 @@
+
+/**
+ * @brief Initializes the Cryptocell 310.
+ *
+ * @return int
+ * @retval 0 on success
+ * @retval non-zero on error
+ */
+int cc310_init(void);
---
+++ cpu/nrf9160/periph/cc310.c
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2021 HAW Hamburg
+ *
+ */
+
+#include "cpu.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "kernel_defines.h"
+
+#include "nrf_cc3xx_platform.h"
+#include "nrf_cc3xx_platform_ctr_drbg.h"
+
+#define ENABLE_DEBUG 1
+#include "debug.h"
+
+int cc310_init(void)
+{
+    int res;
+
+    if (IS_USED(CC310_INTERRUPT)) {
+        /* Enable interrupts */
+        NVIC_EnableIRQ(CRYPTOCELL_IRQn);
+    }
+
+    /* Set the RTOS abort APIs */
+    nrf_cc3xx_platform_abort_init();
+
+    /* Set the RTOS mutex APIs */
+    nrf_cc3xx_platform_mutex_init();
+
+    /* Initialize the cc3xx HW with RNG support */
+    res = nrf_cc3xx_platform_init();
+    if (res)
+    {
+        DEBUG("Could not initialize cc310 platform (%d)\n", res);
+    }
+    else
+    {
+        DEBUG("Initialized cc310 platform\n");
+    }
+
+    return res;
+}
+
+#if IS_USED(CC310_INTERRUPT)
+void isr_cryptocell(void)
+{
+    CRYPTOCELL_IRQHandler();
+}
+#endif
